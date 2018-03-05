ad80057acc3493cc181f52513b1419144b544ca6
bhuvnesh25 <bhuvneshkumar2798@gmail.com>
2018-03-05 18:16:07 +0530

    2 programs completed

---
+++ src/bhuivnesh/even.java
@@ -0,0 +1,24 @@
+package bhuivnesh;
+
+public class even {
+	public static void main(String args[]) {
+	
+		int i;
+		for(i=0;i<100;i++)
+			{
+			if(i%2==0)
+			{
+			System.out.print("even=");
+			System.out.println(i);
+			}
+			else {
+				System.out.print("odd=");
+				System.out.println(i);
+			}
+			
+			}
+			
+		}
+	}
+
+
---
+++ src/bhuivnesh/helloworld.java
@@ -0,0 +1,7 @@
+package bhuivnesh;
+
+public class helloworld {
+   public static void main(String args[]){
+	   System.out.println("hello Bhuvnesh");
+   }
+}
